08d069137d0f45e1a4358b479f0ae38d46439509
PrashantGM <gmprem14@gmail.com>
2022-12-28 18:00:50 +0545

    private field added

--- js-concepts/shape-OOP.js
+++ js-concepts/shape-OOP.js
@@ -20,12 +20,19 @@ class Cuboid extends Shape {
 }
 
 class Cylinder extends Shape {
+  //private data field declared
+  #secret_value;
   constructor(radius, height, type) {
     super(height, type);
     this.radius = radius;
+    this.#secret_value =
+      'The three most important parts of any 3D shape are its edges, faces, and vertices.';
   }
   volume() {
     return (22 / 7) * this.radius * this.height;
+  }
+  tellSecret() {
+    return this.#secret_value;
   }
 }
 
@@ -33,5 +40,11 @@ const cube = new Cuboid(3, 5, 9, 'cube');
 console.log(cube.volume());
 const cylinder = new Cylinder(1.5, 8, 'cylinder');
 console.log(cylinder.volume());
+
+//access superclass with inheritance and override the identify function
 console.log(cube.identify());
 console.log(cylinder.identify());
+
+//access private data through public method
+const newCy = new Cylinder(1, 8, 'cylinder');
+console.log(newCy.tellSecret());
